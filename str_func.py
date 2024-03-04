a0a486c8f82f21e95d1f009a240f6d05119a3757
Dmitry <dima12_yarrin@mail.ru>
2024-03-05 00:19:09 +0300

    Add function title_string

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def uppercase_string(input_string):
+    """Function to convert a uppercase string"""
     return input_string.upper()
+
+def title_strin(input_title):
+    """Function to convert a title string"""
+    return input_title.title()
