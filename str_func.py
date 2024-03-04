dcbad944c92edd95d2c21dea429cae009a86385b
LuckyFox97 <160795044+LuckyFox97@users.noreply.github.com>
2024-03-05 00:03:17 +0300

    Delete str_func.py

--- str_func.py
+++
@@ -1,7 +0,0 @@
-def uppercase_string(input_string):
-    """Function to convert a uppercase string"""
-    return input_string.upper()
-
-def title_string(input_title):
-    """Function to convert a title string"""
-    return input_title.title()
